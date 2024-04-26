d9bbfa39b6f5051b10c7290437a07cd20fbf4e27
idriss kenne <idrisskenne51@gmail.com>
2024-04-26 14:36:09 +0200

    import mod

--- creation1.py
+++ creation1.py
@@ -1,5 +1,14 @@
 #creation de fenetre tkinter
+#import des module
+from tkinter import *
 import tkinter as tk
+from tkinter.filedialog import askopenfilename, asksaveasfilename
+from tkinter.font import Font
+from tkinter import filedialog
+from tkinter import messagebox
+import os
+#creation de fenetre tkinter
+
 bloc = tk.Tk()
 bloc.title(" bloc note ")
 bloc.geometry("700x500")
