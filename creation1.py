de24ab78a7f3edc7ff54ad93af6834e78523bb1a
idriss kenne <idrisskenne51@gmail.com>
2024-04-27 22:38:55 +0200

    creation des fonction 2 et ajout

--- creation1.py
+++ creation1.py
@@ -27,15 +27,71 @@ def newFile():
 #creation de la fonction ouvri un fichier
 
 def openFile():
+    #creation de la variable qui vas contenir le type de document a ouvrir 
     global file
     file = askopenfilename(defaultextension=".txt", filetypes=[("All Files", "*.*"), ("Text Documents", "*.txt")])
     if file == "":
         file = None
     else:
        
-        textArea.delete(1.0, END)
+        textArea.delete(1.0, END) # supprime le contenu de la page ouverte 
         f = open(file)
-        textArea.insert(1.0, f.read())
+        textArea.insert(1.0, f.read()) # ouvre le nouveau contenu souhaiter
+
+#creation de la fonction sauvgardé
+def save():
+    #tenter d'nregistre le cotenu
+    try:
+        file_content = textArea.get("1.0", tk.END)
+        file_path = filedialog.asksaveasfilename(defaultextension=".txt", filetypes=[("Fichiers texte", ".txt"), ("Text files", "*.txt")])
+        if not file_path:
+            return
+        with open(file_path, 'w') as file: 
+            file.write(file_content)
+            messagebox.showinfo("succes", "Le fichier a ete sauvegardé avec succés.")
+    except Exception as e:
+            messagebox.showerror("Erreur", str(e)) #lors que le fichier n'arrive pas etre sauvegardé affiche le message d'erreur
+# creation de la fonction pour quitter la page 
+def quitFile():
+    root.destroy()
+#creation de la fonction couper le texte
+def cut():
+    textArea.event_generate(("<<Cut>>"))
+
+#creation de la fonction copier le texte
+def copy():
+    textArea.event_generate(("<<Copy>>"))
+
+#creation de la fonction coller
+def paste():
+    textArea.event_generate(("<<Paste>>"))
+
+ # fonction info du bloc note   
+def info():
+    messagebox.showinfo("About Bloc note", '''Bloc note
+    
+Version - 1.1.1
+Developper - kenne idriss, christopher, Loic
+    de la premiere année en informatique a L'Ecole IT''')
+
+# changer la taille du texte 
+def changeSizeWindow():
+    def changeSize():
+        global font_size 
+        font_size = size.get()
+        NotePad_font.configure(size=font_size)
+        TextSize.destroy()
+    
+    TextSize = Toplevel()
+    TextSize.geometry("400x100")
+    TextSize.title("tAILLE DE TEXTE")
+
+    size = StringVar()
+    size.set(NotePad_font.cget('size'))
+    
+    Label(TextSize, text="Enter la taille de texte que vous voulez:", font="lucida 15 bold").pack()
+    Entry(TextSize, textvariable=size, font="lucida 15").pack()
+    Button(TextSize, text="Appliqué", command=changeSize).pack()
     
 
 #creation de la zone de texte
@@ -43,7 +99,7 @@ def openFile():
     file = None
     textArea.pack(expand=True,fill="both")
 
-    ScrollBar.config(command=textArea.yview)
+    ScrollBar.config(command=textArea.yview) # creation de la scrollbar
 
     
 #creation des menu et sous menu
